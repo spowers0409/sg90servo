510a5c1bd16dcb28ea24eb2b3b46d8d477901f8a
spowers0409 <odinissecure6@outlook.com>
2022-10-23 08:44:42 -0500

    Code to move servo to 175 degree then back to 0

---
+++ sg90test.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from time import sleep
+from adafruit_servokit import ServoKit
+
+# Set channels to the number of servo channels on your kit.
+# 8 for FeatherWing, 16 for Shield/HAT/Bonnet.
+kit = ServoKit(channels=8)
+
+kit.servo[0].angle = 175
+sleep(1)
+kit.servo[0].angle = 45
+sleep(1)
